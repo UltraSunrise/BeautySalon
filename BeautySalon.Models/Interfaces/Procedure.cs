c0c8df51737c7c0776617d6f50d41836ff0f59d2
Iavor Orlyov <iavor_orlyov@yahoo.com>
2019-03-11 22:17:58 +0200

    #10

---
+++ BeautySalon.Models/Interfaces/Procedure.cs
@@ -0,0 +1,12 @@
+﻿namespace BeautySalon.Models.Interfaces
+{
+    using BeautySalon.Models.Enums;
+
+    public interface Procedure
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        int TimeDuration { get; set; }
+        ProcedureTypes ProcedureType { get; set; }
+    }
+}
