c8dec08830435a289e8aebc67bf394edab4491f0
Iavor Orlyov <iavor_orlyov@yahoo.com>
2019-03-11 22:41:14 +0200

    #8 Added Procedure Table

---
+++ BeautySalon.Models/Procedure.cs
@@ -0,0 +1,12 @@
+﻿namespace BeautySalon.Models
+{
+    using BeautySalon.Models.Enums;
+
+    public class Procedure
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TimeDuration { get; set; }
+        public ProcedureTypes Type { get; set; }
+    }
+}
